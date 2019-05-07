f5013e963e16109aa4b07466f2843acfbe746718
Administrator <Administrator@192.168.0.105>
2019-05-07 11:32:04 +0800

    项目提交

---
+++ amp-dao/src/main/java/org/amp/dao/App.java
@@ -0,0 +1,13 @@
+package org.amp.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ amp-service/src/main/java/org/amp/service/App.java
@@ -0,0 +1,13 @@
+package org.amp.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
